9bf90716117504f5bfd1b67b0a323e99175ad820
tadm123 <drb_40@hotmail.com>
2024-12-19 22:28:44 -0500

    Create 6.c

---
+++ homeworks_code/Ch5_InterludeProcessAPI/6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+
+int main(void)
+{
+    int rc, status, pid;
+    rc = fork();
+
+    if(rc < 0)
+        printf("failed to fork\n");
+    else if(rc == 0){
+        printf("Hello from child (%d)\n", getpid());
+        exit(0);
+    }
+    else{
+        pid = waitpid(rc, &status, WUNTRACED); 
+        if(WIFEXITED(status))
+            printf("normal termination of child (%d), exit status = %d\n", pid, WEXITSTATUS(status));
+        printf("Goodbye from parent (%d)\n", getpid());
+    }
+}
